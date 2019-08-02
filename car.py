201780b02db046ca4def7118c62a8cfb28cfa6c1
sky-666 <50875149+sky-666@users.noreply.github.com>
2019-08-03 00:55:45 +0800

    Add files via upload

---
+++ car.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"}
+response = requests.get(
+    url="https://www.autohome.com.cn/news/",headers=headers)
+#转义
+response.encoding = response.apparent_encoding
+soup = BeautifulSoup(response.text,features="html.parser")
+
+#定位
+# target = soup.find(class_="class") #class标签
+target = soup.find(id="auto-channel-lazyload-article")
+li_list = target.find_all('li')
+
+for i in li_list:
+    a = i.find('a')
+    if a:
+        print('https:',a.attrs.get("href"),sep="")      #去空格
+        txt = a.find("h3").text             #去标签
+        print(txt)
+        img_url = a.find('img').attrs.get("src")
+        print('https:',img_url,sep="")
+
